422b6f77772d16b88412fe35e51d260b16008f31
Sayyedali <sayyedali.tahami@ut.ac.ir>
2024-11-26 15:13:48 +0330

    solving Question2

--- CA3/Q2.py
+++ CA3/Q2.py
@@ -1,40 +1,37 @@
-from itertools import permutations
-
+def count_occurrences(s):
+    count_h = s.count('H')
+    count_p = s.count('P')
+    count_hp = s.count('HP')
+    count_ph = s.count('PH')
+    return count_h, count_p, count_hp, count_ph
 
 def can_form_string(a, b, c, d, s):
-    # Check if the length of s matches the total length of all words
-    if len(s) != a + b + 2 * c + 2 * d:
-        return "NO"
-
-    # Create the list of words
-    words = ['H'] * a + ['P'] * b + ['HP'] * c + ['PH'] * d
-
-    # Try all permutations of the words
-    for perm in permutations(words):
-        # Join the words in this permutation
-        candidate = ''.join(perm)
-
-        # If the joined string matches the target string, return "YES"
-        if candidate == s:
-            return "YES"
-
-    # If no permutation matches, return "NO"
+    count_h, count_p, count_hp, count_ph = count_occurrences(s)
+    used_hp = min(count_hp, c)
+    used_ph = min(count_ph, d)
+    remaining_h = count_h - used_hp - used_ph
+    remaining_p = count_p - used_hp - used_ph
+    if remaining_h == a and remaining_p == b:
+        return "YES"
     return "NO"
 
+def solve_magic_book(t, test_cases):
+    results = []
+    for case in test_cases:
+        a, b, c, d, s = case
+        result = can_form_string(a, b, c, d, s)
+        results.append(result)
+    return results
 
 def main():
-    # Read the number of test cases
     t = int(input())
-
+    test_cases = []
     for _ in range(t):
-        # Read input for each test case
         a, b, c, d = map(int, input().split())
         s = input().strip()
-
-        # Call the function and print the result
-        result = can_form_string(a, b, c, d, s)
-        print(result)
-
+        test_cases.append((a, b, c, d, s))
+    results = solve_magic_book(t, test_cases)
+    print("\n".join(results))
 
 if __name__ == "__main__":
     main()
